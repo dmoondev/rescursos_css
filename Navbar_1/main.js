7aa57c295692e621ce07e02ee3fca2730c3aa377
dmoondev <dmoondev.17@gmail.com>
2024-04-03 03:28:19 -0300

    Navbar

---
+++ Navbar_1/main.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector(".navbar")
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", ()=> {
+    console.log("scroll");
+    var { scrollY } = window;
+    if(scrollY > lastScrollTop){
+        navbar.classList.remove("visible");
+    }
+    else if (scrollY < lastScrollTop){
+        navbar.classList.add("visible");
+    }
+    console.log(scrollY);
+    lastScrollTop = scrollY <= 0 ? 0 : scrollY;
+    }, { passive: true }
+);
